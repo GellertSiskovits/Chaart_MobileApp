096cd02bd9b66a163e9c0ba662c3cca997f92013
gellert.peter <geraldlionb@gmail.com>
2018-04-19 19:40:34 +0300

    View Data Json Format

--- app/src/main/java/com/example/gellert/mobileapp/SecondActivity.java
+++ app/src/main/java/com/example/gellert/mobileapp/SecondActivity.java
@@ -1,13 +1,56 @@
 package com.example.gellert.mobileapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.*;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.*;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button showData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //Button
+
+        showData = (Button) findViewById(R.id.showData);
+
+        //showData Button Click
+
+        showData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), data_dispaly.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/gellert/mobileapp/data_dispaly.java
@@ -0,0 +1,166 @@
+package com.example.gellert.mobileapp;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class data_dispaly extends ListActivity {
+
+    private ProgressDialog pDialog;
+
+    JSONParser  jParser = new JSONParser();
+
+    JSONArray products = null;
+
+    private static String url_all_products = "http://192.168.1.8/AndroidApp/Data/getAllProducts.php";
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "products";
+    private static final String TAG_PID = "ExpenseId";
+    private static final String TAG_NAME = "expense_date";
+    private static final String TAG_DATE = "expense_date";
+    private static final String TAG_NOTE = "expense_note";
+    private static final String TAG_CAT = "expense_category";
+
+
+    ArrayList<HashMap<String,String>> productsList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_dispaly);
+
+        productsList = new ArrayList<HashMap<String, String>>();
+
+        //load data -> Background Thread
+
+        new LoadAllProducts().execute();
+
+        ListView lv = (ListView)findViewById(android.R.id.list);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+    }
+
+    class LoadAllProducts extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(data_dispaly.this);
+            pDialog.setMessage("Loading products. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    products = json.getJSONArray(TAG_PRODUCTS);
+
+                    // looping through All Products
+                    for (int i = 0; i < products.length(); i++) {
+                        JSONObject c = products.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String id = c.getString(TAG_PID);
+                        String name = c.getString(TAG_NAME);
+                        String category = c.getString(TAG_CAT);
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_PID, id);
+                        map.put(TAG_NAME, name);
+                        map.put(TAG_CAT, category);
+
+                        // adding HashList to ArrayList
+                        productsList.add(map);
+                    }
+                } else {
+                    // no products found
+                    // Launch Add New product Activity
+                    Intent i = new Intent(getApplicationContext(),
+                            data_new.class);
+                    // Closing all previous activities
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+//                   /* *//**
+//                     * Updating parsed JSON data into ListView
+//                     * *//*
+                    ListAdapter adapter = new SimpleAdapter(
+                            data_dispaly.this, productsList,
+                            R.layout.list_item, new String[] { TAG_PID,
+                            TAG_NAME,TAG_CAT},
+                            new int[] { R.id.pid, R.id.name });
+                    // updating listview
+
+                    setListAdapter(adapter);
+                }
+            });
+
+        }
+
+    }
+}
+
